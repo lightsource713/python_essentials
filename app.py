95772e01ea8f2c59bb058c7797d82d480955300f
lightSource0713 <kingstone0713@gmail.com>
2022-04-30 00:11:32 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+# Como nomear uma variável
+peso = 500
+altura = 180
+
+# Variáveis de múltiplas palavras
+nome_completo = 'ana beatriz martins'
+
+# Variáveis com uma palavra
+marca = 'dell'
+
+# Como nomear constantes
+PESO = 100
+PESO_MAXIMO = 200
+
+# Como nomear funções
+def desligar_computador():
+ print('desligando computador')
+
+ #como nomar classes - CamelCase
+ class Computador:
+     pass
+class ComputadorGamer:
+    pass
