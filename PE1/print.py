8021ed765dd424849c55be0d20f71dbaeb1965de
lightSource0713 <kingstone0713@gmail.com>
2022-06-19 22:20:58 -0300

    Update print.py

--- PE1/print.py
+++ PE1/print.py
@@ -2,3 +2,15 @@ print('Hello, Wolrd!')
 
 # invocação da função
 function_name(argument)
+
+# newline
+print()
+
+# ex:
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+# The itsy bitsy spider climbed up the waterspout.
+#
+# Down came the rain and washed the spider out.
