a399b4eaec88c8789766d30866de595e6839fe11
lightSource0713 <kingstone0713@gmail.com>
2022-06-22 19:59:14 -0300

    Update print.py

--- PE1/print.py
+++ PE1/print.py
@@ -12,5 +12,68 @@ print()
 print("Down came the rain and washed the spider out.")
 
 # The itsy bitsy spider climbed up the waterspout.
-#
+
 # Down came the rain and washed the spider out.
+
+# caractere de escape (\)
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+# The itsy bitsy spider
+# climbed up the waterspout.
+
+# Down came the rain
+# and washed the spider out.
+
+#  strings
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+# output
+# The itsy bitsy spider climbed up the waterspout.
+
+print("My name is", "Python.")
+print("Monty Python.")
+
+# My name is Python.
+# Monty Python.
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+# My name is Python. Monty Python.
+
+print("My name is ", end="")
+print("Monty Python.")
+
+# My name is Monty Python.
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# My-name-is-Monty-Python.
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+# My_name_is*Monty*Python.*
+
+print("    *\n   * *\n  *   *\n *     *\n*       *\n***   ***\n  *   *\n  *   *\n  *****")
+ 
+#     *
+#    * *
+#   *   *
+#  *     *
+# *       *
+# ***   ***
+#   *   *
+#   *   *
+#   *****
+
+print("string" * 2)
+# stringstring
+
+print(0o123)
+# 83
+
+print(0x123)
+# 291
